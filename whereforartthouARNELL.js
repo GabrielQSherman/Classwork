0ad45df99eefa61b170e27c10215165dc341fe39
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-12 23:54:34 -0500

    Add files via upload

---
+++ whereforartthouARNELL.js
@@ -0,0 +1,18 @@
+var readline = require('readline'),
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    }),
+
+    allObjects = [];
+
+
+function createObject() {
+    rl.question('What do you want your object to be called', (objectName) => {
+
+        let objectName = 
+
+    })
+
+    
+}
