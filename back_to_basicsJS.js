27bded6dd52b71f0b84d82c5e717dc289f3dd765
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-26 15:28:45 -0500

    going back to basic javascript to refresh my mind and cement my foundation

---
+++ back_to_basicsJS.js
@@ -0,0 +1,32 @@
+//1. 
+//This is a single line comment, created with '//' two forward slashes
+
+/* a multi line comment
+
+starts with '/ *' and ends with '* /' (no spaces in between * and /)
+
+ this doesnt need any slashes or indication that it is a comment, it is simply inside a 'comment block'
+
+
+*/
+
+//2.
+
+/*
+one way to declare a variable that will be set at the global scope is using the 'var' keyword
+and then the name of your variable following the key word
+*/
+
+//name will know be a variable in this code, but it will have a value of 'undefined'
+var name;
+
+//3 & 4
+
+//using the assignment operater '=' you can set a value to a variable, so that it is not undefined
+
+//You are able to assign the value when it is initialized or after it has been initialized
+
+var newVar = 3; //this is now an interger variable, but it can be reassined to any value type. 
+
+newVar = 'hello world'; //now myVar is a string and there will be no syntax error
+//you can also reasign a variable to another variables value
