26785f39bea20c32b5fb0cfb9cf391de0248092a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-03 22:54:35 -0500

    update program for user interactivity today I added a function that is called with the onchange envent, specifically for my availablerooms select element
    this function will take the value of the select element, find that specific room, then transfer it into the booked rooms array.
    
    then the select options are updated by removed the select element childnode from its parent node ('selectDiv')
    
    next up I will be doing the same for the booked rooms array/select element

--- Projects/JsHotel/COHORT10/InClassPractice/hotelpractice02.js
+++ Projects/JsHotel/COHORT10/InClassPractice/hotelpractice02.js
@@ -18,14 +18,27 @@ let booked = [107];
 
 const selectDiv = document.getElementById('selectElementsDiv');
 
+
+
+
 create_available_select()
 
 //1. Available Rooms
 function create_available_select() {
 
+    //inital creation of select element
     let availableSelect = document.createElement('select');
 
+    // console.log(availableSelect);
+    availableSelect.onchange = available_select_onchange;
+    //availableSelect not yet accessable outside of this function
+
+    availableSelect.id = 'availableSelectID';
+
+    //default option creation and data transfer to DOM
     let defaultOption = document.createElement('option');
+
+    defaultOption.value = 'default';
 
     defaultOption.innerText = 'Select An Available Room';
 
@@ -41,21 +54,78 @@ function create_available_select() {
 
         let roomOption = document.createElement('option');
 
-        roomOption.innerText = available[i]; //or element
+        roomOption.value = available[i];
+
+        roomOption.innerText = `Room ${available[i]}`; //or element
 
         availableSelect.appendChild(roomOption)
         
     }
 
     
+    
 
     //we will be using onchange event (event listener on ava)
 
     selectDiv.appendChild(availableSelect);
+
+
+}
+
+//function is going to transfer selected to booked rooms
+function available_select_onchange() {
+
+    let selectElm = document.getElementById('availableSelectID');
+
+    //bookedelm 
+
+    // console.log('changed');
+
+    // booked.push(available.splice(0, 1));
+
+    if (selectElm.value == 'default') {
+
+        console.log('That is not a room!');
+        return //exit the function if the default was selected 
+
+    } else {
+
+        console.log(`Room ${selectElm.value} has been selected`);
+        
+    }
+    
+    for (let i = 0; i < available.length; i++) {
+        
+        // console.log(available[i], selectElm.value);
+        
+        if (available[i] == selectElm.value) {            
+
+            booked.push(available.splice(i, 1));
+            
+        }
+        
+    }
+
+    //method 1 (not best practice) delete old, create new
+
+    selectDiv.removeChild(selectElm);
+
+    create_available_select()
+
+    //method 2 (best practice) update old
+
+
+
+    console.log(`Available: ${available}\nBooked: ${booked}`);
+    
 }
 
 
 //2.Booked Rooms
 function create_booked_select() {
+    
+}
+
+function booked_select_onchange() {
     
 }
