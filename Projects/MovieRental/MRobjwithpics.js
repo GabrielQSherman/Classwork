c1c45eabaf2965831280b02b0c52244d7eeacb80
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-20 19:28:08 -0400

    update project

--- Projects/MovieRental/MRobjwithpics.js
+++ Projects/MovieRental/MRobjwithpics.js
@@ -5,10 +5,14 @@ let movieRentalDB = {
 
         {title: 'Looper',             release: 2012, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/0/0a/Looper_poster.jpg' }, 
         {title: 'Back To The Future', release: 1985, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg'}, 
-        {title: 'Inception',          release: 2010, available: false, img: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'}, 
-        {title: 'Donnie Darko',       release: 2001, available: false, img: 'https://m.media-amazon.com/images/M/MV5BOGRiOGM5MmUtMmI3Yi00ZTFhLTlhZDYtZGNmOWRmYTM4NWE2XkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_SY1000_CR0,0,702,1000_AL_.jpg' }, 
-        {title: 'Primer',             release: 2004, available: false, img: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Primer_%282004_film_poster%29.jpg' }, 
-        {title: 'Terminator 2',       release: 1991, available: true, img: 'https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg' }
+        {title: 'Inception',          release: 2010, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'}, 
+        {title: 'Donnie Darko',       release: 2001, available: true, img: 'https://m.media-amazon.com/images/M/MV5BOGRiOGM5MmUtMmI3Yi00ZTFhLTlhZDYtZGNmOWRmYTM4NWE2XkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_SY1000_CR0,0,702,1000_AL_.jpg' }, 
+        {title: 'Primer',             release: 2004, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Primer_%282004_film_poster%29.jpg' }, 
+        {title: 'Terminator 2',       release: 1991, available: true, img: 'https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg' },
+        {title: 'Source Code',        release: 2011, available: false, img: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Source_Code_Poster.jpg/220px-Source_Code_Poster.jpg' },
+        {title: 'Déjà Vu',            release: 2006, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/DejaVuBigPoster.jpg/220px-DejaVuBigPoster.jpg' }
+
+        
     ],
 
     displayAvailable() {
@@ -22,7 +26,7 @@ let movieRentalDB = {
 
             let movieDiv = createDivElm(false, 'movieDiv'),
                 title = createHeadingElm(avlMovie.title, 2),
-                movieImg = createImgElm(avlMovie.img, avlMovie.title + ' Image'),
+                movieImg = createImgElm({id: avlMovie.title, src: avlMovie.img, alt: avlMovie.title + ' Image'}),
                 releaseDate = createHeadingElm('Released In The Year ' + avlMovie.release, 5);
 
                 // console.log(title, movieImg, releaseDate);
@@ -103,17 +107,17 @@ function createDivElm(id, divClass) {
     
 }
 
-function createImgElm(src, alt, id) {
+function createImgElm(imgObj) {
 
     let image = document.createElement('img');
 
-    image.src = src;
+    image.src = imgObj.src;
 
-    image.alt = alt == false ? 'no image' : alt;
+    image.alt = imgObj.alt == false ? 'no image' : imgObj.alt;
 
-    if (id != undefined && document.getElementById(id) == null) {
+    if (imgObj.id != undefined && document.getElementById(imgObj.id) == null) {
 
-        image.id = id;
+        image.id = imgObj.id;
         
     }
 
@@ -143,6 +147,7 @@ document.body.appendChild(rentedDiv);
 document.body.style.background = 'yellow';
 
 availableDiv.style.display = 'flex';
+availableDiv.style.flexWrap = 'wrap';
 availableDiv.style.background = 'pink';
 
 rentedDiv.style.display = 'flex';
