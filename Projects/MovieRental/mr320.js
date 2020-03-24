df30c1b7ba8b9b9594dfae2341ee277b561491c0
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-23 21:25:37 -0400

    worked on creating flex containers of movie information dynamically. created function to create hyperlinks

--- Projects/MovieRental/mr320.js
+++ Projects/MovieRental/mr320.js
@@ -1,38 +1,60 @@
 let movieRental = {
      allMovies: [
 
-        {title: 'Looper',             release: 2012, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/0/0a/Looper_poster.jpg' }, 
-        {title: 'Back To The Future', release: 1985, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg'}, 
-        {title: 'Inception',          release: 2010, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'}, 
-        {title: 'Donnie Darko',       release: 2001, available: true, img: 'https://m.media-amazon.com/images/M/MV5BOGRiOGM5MmUtMmI3Yi00ZTFhLTlhZDYtZGNmOWRmYTM4NWE2XkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_SY1000_CR0,0,702,1000_AL_.jpg' }, 
-        {title: 'Primer',             release: 2004, available: true, img: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Primer_%282004_film_poster%29.jpg' }, 
-        {title: 'Terminator 2',       release: 1991, available: true, img: 'https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg' },
-        {title: 'Source Code',        release: 2011, available: false, img: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Source_Code_Poster.jpg/220px-Source_Code_Poster.jpg' },
-        {title: 'Déjà Vu',            release: 2006, available: false, img: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/DejaVuBigPoster.jpg/220px-DejaVuBigPoster.jpg' }
+        {title: 'Looper',             release: 2012, available: false, imbdLink: 'https://www.imdb.com/title/tt1276104/', img: 'https://upload.wikimedia.org/wikipedia/en/0/0a/Looper_poster.jpg' }, 
+        {title: 'Back To The Future', release: 1985, available: true, imbdLink: 'https://www.imdb.com/title/tt0088763/', img: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg'}, 
+        {title: 'Inception',          release: 2010, available: false, imbdLink: 'https://www.imdb.com/title/tt1375666/', img: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'}, 
+        {title: 'Donnie Darko',       release: 2001, available: true, imbdLink: 'https://www.imdb.com/title/tt0246578/', img: 'https://m.media-amazon.com/images/M/MV5BOGRiOGM5MmUtMmI3Yi00ZTFhLTlhZDYtZGNmOWRmYTM4NWE2XkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_SY1000_CR0,0,702,1000_AL_.jpg' }, 
+        {title: 'Primer',             release: 2004, available: true, imbdLink: 'https://www.imdb.com/title/tt0390384/', img: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Primer_%282004_film_poster%29.jpg' }, 
+        {title: 'Terminator 2',       release: 1991, available: true, imbdLink: 'https://www.imdb.com/title/tt0103064/', img: 'https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg' },
+        {title: 'Source Code',        release: 2011, available: true, imbdLink: 'https://www.imdb.com/title/tt0945513/', img: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Source_Code_Poster.jpg/220px-Source_Code_Poster.jpg' },
+        {title: 'Déjà Vu',            release: 2006, available: false, imbdLink: 'https://www.imdb.com/title/tt0453467/', img: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/DejaVuBigPoster.jpg/220px-DejaVuBigPoster.jpg' }
 
     ],
 
-    displayRented() {
+    displayAllMovies() {
 
-        let arrayOfRented = movieRental.allMovies.filter( object => { return object.available == false});
+        this.allMovies.forEach( singleMovie => {
 
-        // let arrayOfAvailable = movieRental.allMovies.filter( object => { return object.available == true});
+            console.log(singleMovie.img);
 
-        arrayOfRented.forEach( movieObject => {
+            //create html element variable that will be appended to the DOM 
+             let singleMovieDiv = createDivElement({class: 'movies'}),
 
-            console.log(movieObject.title);
+                 movieTitle = createHeading({size: 2, text: singleMovie.title}),
+
+                 movieRD = createHeading({text: `Year Released ${singleMovie.release}`, size: 4}),
+
+                 movieImage = createImg({class: 'movieImages', src: singleMovie.img, alt: singleMovie.title + ' Image'}),
+
+                 IMDBlink = createHyperLink({hrefLink: singleMovie.imbdLink, text: singleMovie.title + ' IMDB Page', class: 'imbdlink'});
+
+            //append to the child elements to the subdiv (one subdiv for each movie)
+            singleMovieDiv.appendChild(movieTitle);
+
+            singleMovieDiv.appendChild(movieImage);
+
             
-            let movieTitleHeading = createHeading({text: movieObject.title, size: 1});
 
-            document.body.appendChild(movieTitleHeading);
+            singleMovieDiv.appendChild(movieRD);
 
-            let movieImage = createImg({src: movieObject.img, alt: movieObject.title+' Image Could Not Load', class: 'movieImages'});
+            singleMovieDiv.appendChild(IMDBlink);
 
-            document.body.appendChild(movieImage)
+            //new variable to determine what div the subdiv(movieDiv) gets appended to 
+            let appendLocation;
 
-            let movieRelease = createHeading({text: movieObject.release.toString(), size: 4});
+            //decide what div the movieDiv should be appened, depends on if its avaialble or not
+            if ( singleMovie.available === true ) {
 
-            document.body.appendChild(movieRelease);
+                appendLocation = 'aDiv';
+
+            } else {
+
+                appendLocation = 'rDiv';
+            }
+
+            //append movie div to one of the main div containers
+            document.getElementById(appendLocation).appendChild(singleMovieDiv);
             
         });
 
@@ -40,14 +62,13 @@ let movieRental = {
 
 }
 
-movieRental.displayRented()
+//Method/Function Calls
+createInitalDivs()
 
-// console.log(movieRental.allMovies);
-//create Html elment method CREATES HEADING ELEMENTS PARAMERTERS: heading-text, heading-size
-
-//return the heading element
+movieRental.displayAllMovies()
 
 
+//FUNCTIONS THAT CREATE HTML ELEMENTS
 function createHeading(headingObj) {
 
     let heading = headingObj.size >= 1 && headingObj.size <= 5 ? document.createElement('h'+ headingObj.size) : document.createElement('h4');
@@ -68,24 +89,88 @@ function createImg(imageObj) {
 
     let image = document.createElement('img');
 
-    image.src = imageObj.src != undefined ? imageObj.src : 'https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg'
-    
-    image.alt = imageObj.alt != undefined ? imageObj.alt : 'image could not load/broken';
+    image.src = imageObj.src != undefined ? imageObj.src : './img.jpeg';
 
-    if (imageObj.id != undefined && document.getElementById(imageObj.id) == null) {
+    image.alt = imageObj.alt != undefined ? imageObj.alt : 'Image Could Not Load';
+
+    if (imageObj.id != undefined && document.getElementById(imageObj.id) == null ) {
 
         image.id = imageObj.id;
+        
     }
 
-    if (imageObj.class != undefined) {
+    if ( imageObj.class != undefined ) {
 
-        image.class = imageObj.class;
+        image.className = imageObj.class;
         
     }
 
     return image 
 }
 
+function createDivElement(divObject) {
 
-// let heading1= createImg({id: 'stuff'})
-// console.log(heading1);
+    //class and id
+
+    const div = document.createElement('div');
+
+    if (divObject.id != undefined && document.getElementById(divObject.id) == null) {
+
+        div.id = divObject.id; 
+        
+    }
+
+    if (divObject.class != undefined ) {
+
+        div.className = divObject.class;
+        
+    }
+
+    console.log(div);
+    
+
+    return div
+    
+}
+
+function createHyperLink(linkObject) {
+
+    //class and id
+
+    const link = document.createElement('a');
+
+    if (linkObject.id != undefined && document.getElementById(linkObject.id) == null) {
+
+        link.id = linkObject.id; 
+        
+    }
+
+    if (linkObject.class != undefined ) {
+
+        link.className = linkObject.class;
+        
+    }
+
+    link.innerText = linkObject.text != undefined ? linkObject.text : 'Untitled Link';
+
+    link.href = linkObject.hrefLink != undefined ? linkObject.hrefLink : 'No Link';
+
+    // console.log(link);
+    
+
+    return link
+    
+}
+
+//create our rented div and available div
+function createInitalDivs() {
+
+    let available = createDivElement({id: 'aDiv', class: 'movieDivs'});
+
+    let rented = createDivElement({id: 'rDiv', class: 'movieDivs'});
+
+    document.body.appendChild(available);
+
+    document.body.appendChild(rented);
+
+}
