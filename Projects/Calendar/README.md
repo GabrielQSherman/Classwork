63f8e46e46064bbf24de66cd84f33c74e3aba3e4
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-04-06 19:22:47 -0400

    first commit for my calendar navigation project

---
+++ Projects/Calendar/README.md
@@ -0,0 +1,9 @@
+4/6/2020
+
+first day working on calendar navigation project
+
+the goal is to create a user-interface that allows a user to select a date easily and quickly
+no dates should be left out, and no dates should be 'impossible' dates ie 'Feb. 29th 2019'
+
+The first goal is to create buttons(controls the date being displayed) and a heading element(displays the date) on an html page using pure JS
+
---
+++ Projects/Calendar/calendar.js
@@ -0,0 +1,16 @@
+/*
+
+[] create my buttons along with on heading elm
+
+[] set my event listeners -> create functions
+
+    one function for each button, 
+    update the date variable/s, 
+    update the heading to reflect the date var
+
+
+[] set the heading text to the current date, store the current date in a variable
+
+[] track numbers of days in a month, how many month ( year is easiest to deal with)
+
+*/
