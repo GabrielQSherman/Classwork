ab2a450fd719c1d9ed03d4133801f064c74c2414
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-05-12 22:16:13 -0400

    implement user data eding with PATCH request

--- Projects/APIs/go-rest-api/index.js
+++ Projects/APIs/go-rest-api/index.js
@@ -6,12 +6,12 @@ window.onload = () => {
 
     let usersDiv = createDivElement({id: 'usersDiv'});
 
-    let prevPage = document.createElement('button');
+    const prevPage = document.createElement('button');
     prevPage.id = 'prevPage';
     prevPage.onclick = prevPageReq;
     prevPage.innerText = 'Previous Page';
 
-    let nextPage = document.createElement('button');
+    const nextPage = document.createElement('button');
     nextPage.id = 'nextPage';
     nextPage.onclick = nextPageReq;
     nextPage.innerText = 'Next Page';
@@ -50,17 +50,38 @@ function displayUserPage(usersData) {
 
     usersDiv.appendChild(pageHeading);
 
-    console.log(usersData);
+    // console.log(usersData);
     for (let i = 0; i < usersData.length; i++) {
 
+            //sub-div
             let div = createDivElement({});
 
             div.id = usersData[i].id;
 
+            //headings
             let nameHeader = createHeading({size: 3, text: `Name: ${usersData[i].first_name} ${usersData[i].last_name}` });
             let dobHeader = createHeading({size: 4, text: `Date of Birth: ${usersData[i].dob}`});
             let emailHeader = createHeading({size: 5, text: `Email: ${usersData[i].email}`});
 
+
+            //inputs
+            let fNameInput = document.createElement('input');
+            fNameInput.placeholder = 'Enter A New First Name';
+            fNameInput.name = 'first_name';
+
+            let lNameInput = document.createElement('input');
+            lNameInput.placeholder = 'Enter A New Last Name';
+            lNameInput.name = 'last_name';
+
+            let dobInput = document.createElement('input');
+            dobInput.placeholder = 'Date-of-Birth Y-M-D';
+            dobInput.name = 'dob';
+
+            let emailInput = document.createElement('input');
+            emailInput.placeholder = 'Enter A New Email';
+            emailInput.name = 'email';
+
+            //buttons shown when NOT in edit mode
             let editBtn = document.createElement('button');
             editBtn.innerText = 'Edit This User';
             editBtn.onclick = editUser;
@@ -69,12 +90,46 @@ function displayUserPage(usersData) {
             deleteBtn.innerText = 'Delete This User'
             deleteBtn.onclick = deleteUser;
 
-            div.appendChild(nameHeader);
-            div.appendChild(dobHeader);
-            div.appendChild(emailHeader);
-            div.appendChild(editBtn);
-            div.appendChild(deleteBtn);
+            //buttons shown when in edit mode
+            let cancelBtn = document.createElement('button');
+            cancelBtn.innerText = 'Cancel Edit';
+            cancelBtn.onclick = cancelEdit;
 
+            let confirmBtn = document.createElement('button');
+            confirmBtn.innerText = 'Confirm Edit'
+            confirmBtn.onclick = confirmEdit;
+
+            //append to subdiv
+
+            let displayDiv = document.createElement('div');
+            let editDiv = document.createElement('div');
+
+            div.appendChild(displayDiv);
+            div.appendChild(editDiv);
+
+            editDiv.style.display = 'none';
+
+            //what displays when NOT in edit mode
+            displayDiv.appendChild(nameHeader);
+            displayDiv.appendChild(dobHeader);
+            displayDiv.appendChild(emailHeader);
+            displayDiv.appendChild(editBtn);
+            displayDiv.appendChild(deleteBtn);
+
+            //what displays when in edit mode
+            editDiv.innerHTML += 'First Name: ';
+            editDiv.appendChild(fNameInput);
+            editDiv.innerHTML += '<br>Last Name: ';
+            editDiv.appendChild(lNameInput);
+            editDiv.innerHTML += '<br>Date of Birth: ';
+            editDiv.appendChild(dobInput);
+            editDiv.innerHTML += '<br>Email: ';
+            editDiv.appendChild(emailInput);
+            editDiv.innerHTML += '<br>';
+            editDiv.appendChild(cancelBtn);
+            editDiv.appendChild(confirmBtn);
+
+            //append subdiv to main div
             usersDiv.appendChild(div);
        
     }
@@ -82,18 +137,25 @@ function displayUserPage(usersData) {
 }
 
 function editUser() {
+
+    let editDiv = this.parentElement.parentElement.lastChild;
+
+    editDiv.style.display = 'inline';
+    
+    //hide/show elements
+    this.parentElement.style.display = 'none';
     
 }
 
 function deleteUser() {
 
-    let userId = this.parentElement.id;
+    let userId = this.parentElement.parentElement.id;
 
     let confirm = prompt('type CONFIRM');
 
     if (confirm != null && confirm.toLowerCase() == 'confirm' ) {
         
-        this.parentElement.remove();
+        this.parentElement.parentElement.remove();
 
         deleteUserRequest(userId);
 
@@ -101,6 +163,76 @@ function deleteUser() {
         alert('The User Was Not Deleted')
     }
 
+}
+
+function cancelEdit() {
+
+    alert('Nothing was edited');
+
+    // console.log(this, this.parentElement,this.parentElement.parentElement);
+
+    let displayDiv = this.parentElement.parentElement.firstChild;
+    let editDiv = this.parentElement;
+
+    displayDiv.style.display = 'inline';
+    editDiv.style.display = 'none';
+
+    // for (let i = 0; i < singleUserDiv.childNodes.length; i++) {
+    //     const divElm = singleUserDiv.childNodes[i];
+
+    //     if (divElm.style.display == 'none') {
+
+    //         divElm.style.display = 'inline'
+            
+    //     } else {
+
+    //         divElm.style.display = 'none'
+            
+    //     }
+        
+    // }
     
+    
+}
+
+function confirmEdit() {
+
+    // console.log(this, this.parentElement,this.parentElement.parentElement);
+
+    let divChildren = this.parentElement.children,
+
+        userId = this.parentElement.parentElement.id,
+
+        updateReqBody = {};
+
+    //itterate through all the elements in 'editDiv', extract input values into a JS object
+    for (const htmlElm of divChildren) {
+
+        if (htmlElm.localName == 'input' && htmlElm.value.trim() != '') { //or .type == 'text'
+            
+            updateReqBody[htmlElm.name] = htmlElm.value.trim();
+            
+        }
+        
+    }
+
+    if (JSON.stringify(updateReqBody) === '{}') {
+        // console.log('no inputs filled');
+        alert('No input were filled, no were changes made.')
+        return
+        
+    } 
+
+    //stringify the object created from the input elements so it can be used in a XHR
+    updateReqBody = JSON.stringify(updateReqBody);
+
+    updateUserRequest(userId, updateReqBody); //call the PATCH request
+
+    //switch back to the display elements showing
+    let displayDiv = this.parentElement.parentElement.firstChild;
+    let editDiv = this.parentElement;
+
+    displayDiv.style.display = 'inline';
+    editDiv.style.display = 'none';
     
 }
