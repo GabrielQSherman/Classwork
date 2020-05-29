a8bcca8824b174b1ce2057c70bd42913657ab980
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-05-28 22:23:26 -0400

    implemented POST request on the /movie route

--- Projects/APIs/Back-End Only/Intro to Express/routes/movieRouter.js
+++ Projects/APIs/Back-End Only/Intro to Express/routes/movieRouter.js
@@ -19,17 +19,7 @@ const dbRead = require('../middleware/readDB');
 
 const router = express.Router();
 
-router.get('/', dbRead, (req, res) => {
-    //send json with all the movie docs
-    console.log(req.dbData);
-
-    res.json({
-        status: 200,
-        all_movies: req.dbData.movies
-    })
-    
-})
-
+const textFile = process.cwd() + '/database/database.txt';
 
 router.get('/', dbRead, (req, res) => {
     //send json with all the movie docs
@@ -42,9 +32,21 @@ router.get('/', dbRead, (req, res) => {
     
 })
 
-router.get('/:id', dbRead, validateReq, (req, res) => {
 
-    return res.json({
+router.get('/', dbRead, (req, res) => {
+    //send json with all the movie docs
+    console.log(req.dbData);
+
+    res.json({
+        status: 200,
+        all_movies: req.dbData.movies
+    })
+    
+})
+
+router.get('/:id', dbRead, validDB, (req, res) => {
+
+    return res.status(200).json({
         status: 200,
         movie: req.found
     })
@@ -53,19 +55,43 @@ router.get('/:id', dbRead, validateReq, (req, res) => {
 
 //post a new movie
 
+router.post('/', dbRead, validNewMov, (req, res) => {
+
+
+    console.log(req.body);
+
+    // update the DB in JSON
+
+    newDataBaseData = req.dbData;
+
+    newDataBaseData.movies.push(req.body)
+
+    // convert DB to string and writeFile
+
+    newDataBaseData = JSON.stringify(newDataBaseData);
+
+    fs.writeFileSync(textFile, newDataBaseData);
+    // respond with status 200 and the new movie object
+
+    res.status(200).json({
+        status: 200,
+        message: 'posted successfully',
+        new_movie: 'movieObj'
+    })
+    
+})
+
 //update movie
 
 //delete a movie
 
-router.delete('/:id', dbRead, validateReq, (req, res) => {
+router.delete('/:id', dbRead, validDB, (req, res) => {
 
     let databaseData = req.dbData;
 
     databaseData.movies.splice(req.params.id-1, 1);
 
     databaseData = JSON.stringify(databaseData);
-
-    let textFile = process.cwd() + '/database/database.txt';
 
     fs.writeFileSync(textFile, databaseData);
 
@@ -76,7 +102,68 @@ router.delete('/:id', dbRead, validateReq, (req, res) => {
 
 })
 
-function validateReq(req, res, next) {
+//middleware functions for just the movieRouter
+
+function validNewMov (req, res, next) {
+    //get the new movie in an JS object
+
+    console.log(req.body);
+
+    //check for title, release, available, imdbLink, img
+
+    const { title: t, release: r, available, imdbLink, img } = req.body,
+
+    newMovObj = {
+        title: t,
+        release: r,
+        available: available,
+        imdbLink: imdbLink,
+        img: img,
+    },
+    
+    bodyLen = Object.keys(req.body).length,
+
+    newMovObjLength = Object.keys(newMovObj).length;
+
+    if (bodyLen < newMovObjLength || bodyLen > newMovObjLength + 20) {
+
+        res.status(400).json({
+            status: 400,
+            message: 'Bad Request, there were too few or too many key/value pairs in the request body',
+            your_body_length: bodyLen,
+            required_body_length: newMovObjLength
+        });
+
+        return
+        
+    }
+
+    let missingKeys = [];
+
+    for (const k in newMovObj) {
+        if (newMovObj[k] == undefined ) {
+           
+            missingKeys.push(k)
+
+        }
+    }
+
+    if (missingKeys.length > 0) {
+        
+        res.status(400).json({
+            status: 400,
+            error: 'Missing Keys',
+            message: `The request body was missing the keys; ${missingKeys}`
+        })
+        return 
+    }
+
+    req.body = newMovObj;
+
+    next()
+}
+
+function validDB(req, res, next) {
     
     if (!req.dbData.movies) {
         return res.status(500).json({
