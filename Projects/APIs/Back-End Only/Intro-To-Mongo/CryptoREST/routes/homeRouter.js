cb5adc75ced5487b9df3907b9bbf66225824447e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-06-07 18:24:28 -0400

    update homerouter to use pug template if user is not signed in

--- Projects/APIs/Back-End Only/Intro-To-Mongo/CryptoREST/routes/homeRouter.js
+++ Projects/APIs/Back-End Only/Intro-To-Mongo/CryptoREST/routes/homeRouter.js
@@ -1,15 +1,29 @@
-const express = require('express');
+const 
+      //PACKAGES
+      express = require('express');
 
       router = express(),
+      
+      //IMPORTED VARS & FUNCTIONS
+      serverPort = require('../app').port,
 
-      serverPort = require('../app').port; 
+      isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 
-    res.json({
-        crypto: `http://localhost:${serverPort}/crypto`,
-        user: `http://localhost:${serverPort}/user`
-    })
+    let loggedin = await isLoggedIn(req);
+
+    if (loggedin) {
+        res.json({
+            crypto: `http://localhost:${serverPort}/crypto`,
+            user: `http://localhost:${serverPort}/user`
+        })
+
+    } else {
+        res.render('home')
+    }
+
+    
 })
 
 module.exports = router;
