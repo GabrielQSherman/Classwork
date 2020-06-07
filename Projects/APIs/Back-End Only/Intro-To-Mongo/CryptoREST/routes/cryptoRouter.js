89640017f3fc18ee1008314d3054d70d1ec5a383
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-06-06 23:59:29 -0400

    update crypto router

--- Projects/APIs/Back-End Only/Intro-To-Mongo/CryptoREST/routes/cryptoRouter.js
+++ Projects/APIs/Back-End Only/Intro-To-Mongo/CryptoREST/routes/cryptoRouter.js
@@ -1,12 +1,26 @@
-const express = require('express');
+//IMPORTING...
+const 
+      //PACKAGES
+      express = require('express');
 
-      router = express();
+      router = express(),
+
+      //MIDDLEWARE
+      verify = require('../middleware/verifyToken'),
+
+      findUser = require('../middleware/findUser');
 
 //Show all crypto prices 
-router.get('/', (req, res) => {
+router.get('/', verify, findUser, (req, res) => {
+
+      const cryptos = req.userFound.cryptos;
+      
+      if (!cryptos) {
+           res.status(404).json({message:'you have not favorite cryptos'}) 
+      }
 
       res.json({
-            message: 'Top Ten Crypto-Blockchain Technologies',
+            message: 'You Favorite Cryptocurrencies',
 
             cryptos: {
                   bitcoin: 9779,
