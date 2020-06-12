c7868f5b21ba3c81711cfc89fa5589372a483238
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-06-11 23:59:47 -0400

    update index

--- Projects/APIs/Back-End Only/Intro-To-Mongo/movie_rentalREST/index.js
+++ Projects/APIs/Back-End Only/Intro-To-Mongo/movie_rentalREST/index.js
@@ -1,33 +1,31 @@
+///import env vars
 require('dotenv').config();
-//vars
-const express = require('express')
-      server = express(),
-      morgan = require('morgan'),
-      mongoose = require('mongoose'),
-      port = process.env.PORT || 3001,
-      deprecatedObj = { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true},
-      connectionURI = process.env.MONGO,
-      homeRouter = require('./routes/homeRouter'),
-      movieRouter = require('./routes/movieRouter')
+//global variables for this server
+const 
+express = require('express')
+server = express(),
+morgan = require('morgan'),
+mongoose = require('mongoose'),
+port = process.env.PORT || 3001,
+deprecatedObj = { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true},
+connectionURI = process.env.MONGO,
+homeRouter = require('./routes/homeRouter'),
+movieRouter = require('./routes/movieRouter');
 //some middleware needs to go before others
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(express.static('./public'));
-
 //express.json and morgan must be called before in expressInstance.use()
 server.use('/', homeRouter);
 server.use('/movie', movieRouter);
 //connect to mongoose
 mongoose.connect(connectionURI, deprecatedObj, () => {
-    
     console.log('The Server Is Connected To The Database');
-    
 })
 //if err
 mongoose.connection.on('error', (err) => {
 
-    console.log('An error occured trying to connect to MongoDB, Error: ' + err);
-    
+    console.log('An error occured trying to connect to MongoDB, Error: ' + err);    
 })
 //when connection
 mongoose.connection.on('connected', () => {
