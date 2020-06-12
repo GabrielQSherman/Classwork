dcd5a66531ad85351f7011cc2c3bc1bdfaed95c1
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-06-11 23:54:17 -0400

    display request from own api

--- Projects/APIs/Back-End Only/Intro-To-Mongo/movie_rentalREST/public/home.js
+++ Projects/APIs/Back-End Only/Intro-To-Mongo/movie_rentalREST/public/home.js
@@ -1,26 +1,27 @@
 window.onload = () => {
 
-    const xhr = new XMLHttpRequest(),
-          endpoint = `${location}movie/all`;
+    devStart()
+    
+}
+function devStart() {
 
-          
+    const xhr = new XMLHttpRequest(),
+    
+    endpoint = `${location}movie/all`;
+      
     xhr.open('GET', endpoint, true);
 
     xhr.onload = () => {
 
         const response = JSON.parse(xhr.responseText);
-
-
         //display data to DOM with function
         console.log(response);
-        displayMovies(response.movies)
-        
-    }
-    
+        createInitalElements()
+
     xhr.send()
 
+    console.log("Thank you API for allowing me to not write html code!\n");
 }
-
 function displayMovies(allMovies) {
     
     for (let i = 0; i < allMovies.length; i++) {
@@ -44,4 +45,36 @@ function displayMovies(allMovies) {
         document.body.appendChild(movieImg)
 
     }
+}
+
+
+
+function createInitalElements (){
+    const avlDiv = document.createElement('div'),
+    rntDiv = document.createElement('div'),
+    avlBtn = document.createElement('button'),
+    rntBtn = document.createElement('button'),
+    randomBtn = document.createElement('button');
+
+    avlDiv.id = 'avldiv';
+    rntDiv.id = 'rntdiv';
+    avlBtn.id = 'avlbtn';
+    rntBtn.id = 'rntbtn';
+    randomBtn.id = 'testbtn';
+    avlBtn.innerText = 'See Movie Choices';
+    rntBtn.innerText = 'See What Others Are Watching Now';
+    randomBtn.innerText = 'testing'
+    avlBtn.onclick = createAvlDsply;
+    rntBtn.onclick = createRntDsply;
+    randomBtn.onclick = rentRandomMov;
+    avlDiv.style.backgroundColor = 'pink';
+    rntDiv.style.backgroundColor = 'lightgreen';
+    avlDiv.style.textAlign = 'center';
+    document.body.appendChild(avlDiv);
+    document.body.appendChild(avlBtn);
+    document.body.appendChild(rntDiv);
+    document.body.appendChild(rntBtn);
+    document.body.appendChild(randomBtn);
+    }
+    displayMovies(response.movies)
 }
