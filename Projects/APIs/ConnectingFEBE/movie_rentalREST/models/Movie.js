666cf196492254f03c7ab61bf3b29df4c02f937b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-06-15 23:30:42 -0400

    Schema model for Movie collection

---
+++ Projects/APIs/ConnectingFEBE/movie_rentalREST/models/Movie.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const Movie = new mongoose.Schema({
+
+    available: {
+        required: true,
+        type: Boolean
+    },
+
+    title: {
+        unique: true,
+        required: true,
+        type: String 
+    },
+
+    release: {
+        required: true,
+        type: Number
+    },
+
+    imdb_link: {
+        required: true,
+        type: String,
+        validate: (value) => {
+            
+            const urlTest = !validator.isURL(value),
+
+                  imdbTest = /imdb/;
+
+            if (urlTest && imdbTest.test(value)) { //if the URL was not vaild
+
+                throw new Error('IMDB link was invalid')
+                
+            }
+
+        }
+    },
+
+    img: {
+        required: true,
+        type: String,
+        validate: (value) => {
+
+            const test = !validator.isURL(value)
+            
+            if ( test ) {
+                throw new Error('Image Link Not Valid')
+            }
+        }
+    },
+
+    inventory: {
+
+        required: false,
+
+        type: Object,
+
+        default: {
+
+            available: {type: Number, default: 0},
+            rented: {type: Number, default: 0 }
+        }
+
+    }
+
+})
+
+module.exports = mongoose.model('Movie', Movie);//what we export
