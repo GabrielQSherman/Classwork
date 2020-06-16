7109e085b869a26b33c5171d36f6939ee57480ef
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-06-15 23:28:59 -0400

    main server file

---
+++ Projects/APIs/ConnectingFEBE/movie_rentalREST/index.js
@@ -0,0 +1,43 @@
+///import env vars
+require('dotenv').config();
+//global variables for this server
+const 
+express = require('express')
+server = express(),
+morgan = require('morgan'),
+mongoose = require('mongoose'),
+port = process.env.PORT || 3001,
+deprecatedObj = { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true},
+connectionURI = process.env.MONGO,
+homeRouter = require('./routes/homeRouter'),
+movieRouter = require('./routes/movieRouter');
+//some middleware needs to go before others
+server.use(morgan('dev'));
+server.use(express.json());
+
+// server.use(express.static('./public'));
+
+server.use('/', express.static('./public/homeStatic'))
+
+//express.json and morgan must be called before in expressInstance.use()
+server.use('/', homeRouter);
+server.use('/movie', movieRouter);
+//connect to mongoose
+mongoose.connect(connectionURI, deprecatedObj, () => {
+    console.log('The Server Is Connected To The Database');
+})
+//if err
+mongoose.connection.on('error', (err) => {
+
+    console.log('An error occured trying to connect to MongoDB, Error: ' + err);    
+})
+//when connection
+mongoose.connection.on('connected', () => {
+
+    console.log('The server is attempting to connect to the database...');
+    
+})
+//listen
+server.listen( port, () => {
+    console.log(`Server Listening on Port: ${port}`);
+})
