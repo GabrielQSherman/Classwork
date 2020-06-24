520d2fd585aae56f04d5e92e2a61a97b9b4ce849
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-06-23 22:28:38 -0400

    implement patch request on client facing frontend, ittegrate a form in each movie display

--- Projects/APIs/ConnectingFEBE/movie_rentalREST/public/home-pug.js
+++ Projects/APIs/ConnectingFEBE/movie_rentalREST/public/home-pug.js
@@ -1,15 +1,64 @@
 
 window.onload = () => {
+
+    setEventListeners()
+
+    const editDivs = document.getElementsByClassName('editMovie');
+
+        for (const div of editDivs) { div.style.display = 'none'};
+
+    const displayDivs = document.getElementsByClassName('displayMovie');
+
+        for (const div of displayDivs) { div.style.display = 'flex'};
+
     
+    allMovies.style.display = 'flex'; //display will be none while images and elements load
+
+}
+
+function setEventListeners() {
+
     const getButtons = document.getElementsByClassName('getMovie');
 
-    for (const button of getButtons) { button.onclick = reqSingleMovieData };
+        for (const button of getButtons) { button.onclick = reqSingleMovieData };
 
     const deleteButtons = document.getElementsByClassName('deleteMovie');
 
-    for (const btn of deleteButtons) { btn.onclick = deleteSingleMovie};
-    
+        for (const btn of deleteButtons) { btn.onclick = deleteSingleMovie};
 
+    const editViewButtons = document.getElementsByClassName('editMovieBtn');
+
+        for (const button of editViewButtons) { button.onclick = changeEditView};
+
+    const editSubmitButtons = document.getElementsByClassName('submitEdit');
+
+        for (const button of editSubmitButtons) { button.onclick = submitEditReq};
+    
+}
+
+function submitEditReq() {
+    
+    const movieId = this.parentElement.parentElement.parentElement.id;
+    const form = this.parentElement;
+
+    let reqBody = {};
+
+    for (const input of form) {
+
+        let inputValue = input.value.trim();
+
+        console.log(inputValue);
+        
+        if (inputValue != '') {
+
+            reqBody[input.name] = inputValue;
+        
+        }
+
+    }
+
+    console.log(reqBody);
+    
 }
 
 function reqSingleMovieData () {
@@ -31,7 +80,12 @@ function reqSingleMovieData () {
           endpoint = `${location.origin}/movie/${movieId}`;
 
     fetch(endpoint)
-    .then(rs => {return rs.json()})
+    .then(rs => {
+        console.log(rs);
+        
+        
+        return rs.json()
+    })
     .then(res => {
 
         console.log(res);
@@ -42,7 +96,13 @@ function reqSingleMovieData () {
 
 function deleteSingleMovie() {
 
-    const movieId = this.parentElement.id,
+    //prompt user to prevent accidental deletion
+    let confirm = prompt('Type "confirm" to delete document');
+
+    //check if the user typed confirm
+    if (confirm != 'confirm') return alert('Document Deletion Canceled');
+
+    const movieId = this.parentElement.parentElement.id,
 
           endpoint = `${location.origin}/movie/delete/${movieId}`,
 
@@ -51,9 +111,20 @@ function deleteSingleMovie() {
           };
 
     fetch(endpoint, reqObj)
-    .then(rs => {return rs.json()})
-    .then(res => {
+    .then(rs => {
         
+        if (rs.status === 200) { //doc DB deletion success, delete element from FE
+            
+            this.parentElement.parentElement.remove();
+
+        } else { //alert the user that the req was unsuccessful
+
+            const resMsg = "An error occured trying to deleted movie from DB";
+
+            alert(resMsg)
+
+        }
+    
     })
 
     // const movieId = this.parentElement.id,
@@ -65,5 +136,25 @@ function deleteSingleMovie() {
     //     console.log(res);
     // }
     // xhr.send()
+    
+}
+
+function changeEditView() {
+    
+    // console.log(this.parentElement.parentElement);
+    const movieDivChildren = this.parentElement.parentElement.childNodes;
+
+    movieDivChildren.forEach( node => {
+
+        if (node.className === 'displayMovie' || node.className === 'editMovie') {
+
+            node.style.display = node.style.display == 'none' ? 'flex' : 'none';
+            
+        }
+        
+    });
+
+    // console.log(movieDivChildren);
+    
     
 }
