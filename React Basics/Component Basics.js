b6e30c3364d520707ede10f94543c2c134a9fdcd
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-02 20:16:14 -0400

    how to create a form and handle submition with state

--- React Basics/Component Basics.js
+++ React Basics/Component Basics.js
@@ -414,9 +414,53 @@ class ControlledInput extends React.Component {
   render() {
     return (
       <div>
-        <input onChange={this.handleChange} />
+        <input 
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
         <h4>Controlled Input:</h4>
         <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
+
+//how to create a form and implelment submit
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState( (state) => ({
+      submit: state.input
+    }));
+
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
+          <button type='submit'>Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
+        
       </div>
     );
   }
