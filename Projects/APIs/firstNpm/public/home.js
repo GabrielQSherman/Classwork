b509e47a01bca052c943b3d52baa2907ce711421
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-04 21:44:24 -0400

    create xhr that will request a new user being made, send form data as JSON

--- Projects/APIs/firstNpm/public/home.js
+++ Projects/APIs/firstNpm/public/home.js
@@ -1,5 +1,3 @@
-
-
 window.onload = () => {
     //create a form
     
@@ -18,21 +16,34 @@ window.onload = () => {
     //add id property for each element, className for the inputs
     //set event listener for the submit button, log 'Submiting...', 
     //log the form/childElements one by one
+    form.id = 'form'
 
     heading.innerText = 'Register Today!!';
+    heading.id = 'heading';
     
     emailInput.placeholder = 'Enter Email';
     emailInput.type = 'email';
-    
+    emailInput.class = 'input';
+    emailInput.name = 'email';
+
     usernameInput.placeholder = 'Enter Username';
-    
+    usernameInput.class = 'input';
+    usernameInput.name = 'username';
+
     passInput.placeholder = 'Enter Password'
     passInput.type = 'password';
+    passInput.class = 'input';
+    passInput.name = 'password';
     
     passConfirmInput.placeholder = 'Enter Password Again';
     passConfirmInput.type = 'password';
+    passConfirmInput.class = 'input';
+    passConfirmInput.name = 'password2';
 
-    submitBtn.innerText = 'Register'
+    submitBtn.innerText = 'Register';
+    submitBtn.id = 'submitbtn';
+    // submitBtn.onclick = 
+    submitBtn.onclick = submitReg;
 
     //3) append to the DOM
     document.body.appendChild(mainDiv);
@@ -43,5 +54,34 @@ window.onload = () => {
     form.appendChild(usernameInput);
     form.appendChild(passInput);
     form.appendChild(passConfirmInput);
+
+}
+
+function submitReg() {
+    
+    const formElm = document.getElementById('form');
+
+    const reqBody = {};
+
+    for (const input of formElm) {
+
+        reqBody[input.name] = input.value;
+        
+    }
+
+    const endpoint = location.origin + '/user/post/new';
+
+    // //XHR
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:3000/user/post/new');
+    xhr.onload = () => {
+        console.log(xhr.responseText);
+        const res = JSON.parse(xhr.responseText);
+        console.log(res);
+    }
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(JSON.stringify(reqBody))
 
 }
