aa983aad2fa57605c053e3988eff52c2cb128566
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-11 22:26:07 -0400

    implement MongoDb by connecting server to mongo server via mongoose

--- Projects/APIs/firstNpm/app.js
+++ Projects/APIs/firstNpm/app.js
@@ -11,6 +11,7 @@ const userRouter = require('./routes/userRouters');
 
 // CONSTANTS
 const port = process.env.PORT || 3000;
+const URI = process.env.MONGO;
 //MAIN APPLICATION VAR
 const app = express();
 
@@ -26,6 +27,30 @@ app.use(firstMid);
 //USING A ROUTER
 app.use('/', homeRouter);
 app.use('/user', userRouter);
+
+if (typeof URI === 'string') {
+    
+    const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+    //CONNECTION TO DATABASE
+    mongoose.connect(URI, mongoOptions, (err) => {
+        
+        if (err) {
+
+            console.error( `\nError Connecting To MongoDB: ${err.message || err}\n`);
+
+        } else {
+            console.log('Server Connected To DB') 
+        }
+
+    })
+
+
+} else {
+
+    console.error('Mongo URI missing or invalid, check node enviorment variables');
+    
+}
 
 //START SERVER LISTENING
 app.listen(port, () => {
