ed9d11ea5cd83e6a3e7090e4798522d8b2e5e26e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-13 22:12:34 -0400

    create User model for first collection, add in some minor validation constraints

---
+++ Projects/APIs/firstNpm/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 33,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7
+    },
+    email:{
+        type: String,
+        required: true,
+        minlength: 6,
+        maxLength: 200,
+        unique: true,
+    },
+    emailValidated:{
+        type:Boolean,
+        default: false
+    }
+})
+
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
