97c57c1bb5ae353234886a48779a4772b2d9965f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-24 23:18:24 -0400

    add morgan middleware that will log each reques's basic info

--- Projects/APIs/firstNpm/app.js
+++ Projects/APIs/firstNpm/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 //PACKAGES
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 //MIDDLES
 const firstMid = require('./middleware/firstMiddleware');
 //ROUTERS
@@ -19,10 +20,10 @@ const app = express();
 
 // MIDDLEWARE THAT ALL ROUTES USE 
 // (regardless of request-path or method) '/' /username
-app.use(express.json());
 app.use(express.static('public'));
-
+app.use(express.json());
 app.use(firstMid);
+app.use(morgan('dev'));
 
 //USING A ROUTER
 app.use('/', homeRouter);
