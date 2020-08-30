1f76de73c076f17e88a039aabec84b2b0e91ef3f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-30 11:56:47 -0400

    create route handler for user registration

--- Projects/APIs/ConnectingFEBE/soloFullStack/routes/userRouter.js
+++ Projects/APIs/ConnectingFEBE/soloFullStack/routes/userRouter.js
@@ -1,9 +1,37 @@
 const router = require('express').Router();
 
+const validateUser = require('../middleware/validateUser');
+
+const User = require('../models/User');
+
+//test route for user router
 router.get('/', (req, res) => {
 
     res.send('trying to get users?')
 
 })
+
+//@path: POST *server*/user/register
+//@desc: handels user registration, will upload a new User document to MongoDB.
+//@access: public 
+router.get(
+    '/register', 
+    validateUser, 
+    (req, res) => {
+        try {
+
+            //req.userData is defined by the validation middleware
+            const newUser = await User.create(req.userData);
+            res.status(201).json(newUser);
+
+        } catch (err) {
+            res.status(500).json({
+                message: err.message,
+                error: err
+            })
+        }
+    }
+
+)
 
 module.exports = router
