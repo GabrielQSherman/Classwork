3f11952c3495e80029a1a2f3eda44fbd5121ba06
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-02 17:27:22 -0400

    working with more than one action types in the reducer

--- React Basics/ReduxBasics.js
+++ React Basics/ReduxBasics.js
@@ -62,3 +62,39 @@ const reducer = (state = defaultState, action) => {
   return newState
 };
 
+//this is an example of using the switch statment for multiple possible actions
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+ switch (action.type) {
+    case "LOGIN":
+      return {
+        authenticated: true
+      };
+
+    case "LOGOUT":
+      return {
+        authenticated: false
+      };
+
+    default:
+      return defaultState;
+ }
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
+
