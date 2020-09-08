013236078772817f1187dfd174a5a6d5b095c48f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-07 22:11:28 -0400

    create middleware to check the clients login credentials to make sure they match

---
+++ Projects/APIs/firstNpm/middleware/checkUserCred.js
@@ -0,0 +1,53 @@
+const validate = require('validator').default;
+
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+
+module.exports = async ( req, res, next ) => {
+
+    try {
+
+        const { password: p, credential: c } = req.body;
+
+        p = p.trim();
+        c = c.trim();
+
+        //first is the user logging in with their email or their username?
+        const field = validate.isEmail(c) ? 'email' : 'username';
+        
+        //if email? check that the email is in use
+        //if username? check that the username is in use
+        const query = {};
+        query[field] = c;
+        
+        const projection = {password: 1};
+
+        const foundUser = await User.findOne(query, projection); //return obj with two props. password & _id
+
+        if (foundUser == null) {
+            return res.status(400).json({message: 'Credentials Do Not Match'})
+        }
+        
+        const passwordMatches = bcrypt.compare(p, foundUser.password); //return a boolen (true if the password matches)
+
+        //if pass matches, then go to next middleware, otherwise send a general message 'failed login'
+        if (!passwordMatches) {
+            return res.status(400).json({message: 'Credentials Do Not Match'})
+        }
+
+        //at this point we have confirmed the user's credentials matches and they can be logged in
+        req.userId = foundUser._id;
+        
+        next()
+
+        
+    } catch (err) {
+        console.error(err.message || err)
+        res.json({
+            message: err.message,
+            error: err
+        })
+    }
+
+}
