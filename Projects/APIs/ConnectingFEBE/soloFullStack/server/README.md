f9c8a8c0f86cda3113c44a04d0b5e52206454869
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-17 23:00:29 -0400

    move server files to dedicated folder for organization

---
+++ Projects/APIs/ConnectingFEBE/soloFullStack/server/README.md
@@ -0,0 +1,3 @@
+# Creating a Full MEN Stack Solo Challenge. 
+
+## Starting off with a User login/logout service. And I intend on using JWT and Bcrypt for encrtyption
---
+++ Projects/APIs/ConnectingFEBE/soloFullStack/server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+
+    email: {
+        required: true,
+        type: String,
+        minlength: 6,
+        maxlength: 254,
+        unique: true
+    },
+
+    username: {
+        required: true,
+        type: String,
+        unique: true,
+        minlength: 3,
+        maxlength: 21
+    },
+
+    password: {
+        required: true,
+        type: String,
+        minlength: 7,
+        maxlength: 1000
+    },
+
+    adminProp: {
+        adminLevel: {
+            type: Number,
+            default: 0
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+});
+
+module.exports = mongoose.model("user", User );
