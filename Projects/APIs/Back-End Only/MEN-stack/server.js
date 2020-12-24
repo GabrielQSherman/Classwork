1c5c5122a363e1a9bed4ed2151189ba3ca267047
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-23 23:19:17 -0500

    implement nasa route

--- Projects/APIs/Back-End Only/MEN-stack/server.js
+++ Projects/APIs/Back-End Only/MEN-stack/server.js
@@ -4,13 +4,12 @@ const server = express();
 
 const morgan = require('morgan');
 const firstMiddleware = require('./middlewares/firstMiddleware')
+const nasaRouter = require('./routes/nasa');
 
 server.use(morgan('dev'))
-server.use(firstMiddleware);
+// server.use(firstMiddleware)
 
-server.get("/test", (request, response) => {
-  response.send("Testing Testing 1, 2, 3")
-})
+server.use('/nasa', nasaRouter)
 
 server.listen(3003, () => {
   console.log('listening on port 3003')
