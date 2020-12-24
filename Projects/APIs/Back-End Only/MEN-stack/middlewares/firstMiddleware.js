c7a26be89460281ed58b481b9a7d0b8e51f17693
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-23 23:35:12 -0500

    add axios for http request and implement dotenv, update custom middleware

--- Projects/APIs/Back-End Only/MEN-stack/middlewares/firstMiddleware.js
+++ Projects/APIs/Back-End Only/MEN-stack/middlewares/firstMiddleware.js
@@ -1,4 +1,4 @@
 module.exports = (req, res, next) => {
-  console.log("First Middleware Function!!");
+  console.log("Request Recieved");
   next()
 }
--- Projects/APIs/Back-End Only/MEN-stack/server.js
+++ Projects/APIs/Back-End Only/MEN-stack/server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const PORT = process.env.PORT;
+
 const express = require("express");
 
 const server = express();
@@ -7,10 +10,10 @@ const firstMiddleware = require('./middlewares/firstMiddleware')
 const nasaRouter = require('./routes/nasa');
 
 server.use(morgan('dev'))
-// server.use(firstMiddleware)
+server.use(firstMiddleware)
 
 server.use('/nasa', nasaRouter)
 
 server.listen(3003, () => {
-  console.log('listening on port 3003')
+  console.log('listening on port ' + PORT)
 })
