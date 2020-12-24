09c10cf7dcd4d3374ec82ee7fc4ec20c07af78a8
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-23 23:35:35 -0500

    make today route fetch nasa apod data for the day

--- Projects/APIs/Back-End Only/MEN-stack/routes/nasa.js
+++ Projects/APIs/Back-End Only/MEN-stack/routes/nasa.js
@@ -1,8 +1,22 @@
 
-const router = require('express').Router();
+const router = require('express').Router()
+const {default: axios} = require('axios')
 
-router.get('/today', (req, res) => {
-  res.send(new Date().toISOString().substring(0,10))
+const nasaEndpoint = 'https://api.nasa.gov/planetary/apod'
+
+const nasaKey = process.env.NASA_KEY;
+
+router.get('/today', async (req, res) => {
+  try {
+    const today = new Date().toISOString().substring(0,10);
+
+    const data = await axios.get(`${nasaEndpoint}?date=${today}&api_key=${nasaKey}`)
+  
+    res.json(data)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error: "server error"})
+  }
 });
 
 module.exports = router;
