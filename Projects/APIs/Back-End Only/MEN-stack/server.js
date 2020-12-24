576e733dc7f6c061a9f28ed137f17fb67bdc4927
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-23 23:15:17 -0500

    implement morgan and custom middleware

--- Projects/APIs/Back-End Only/MEN-stack/server.js
+++ Projects/APIs/Back-End Only/MEN-stack/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const server = express();
 
+const morgan = require('morgan');
+const firstMiddleware = require('./middlewares/firstMiddleware')
+
+server.use(morgan('dev'))
+server.use(firstMiddleware);
+
 server.get("/test", (request, response) => {
-
-  response.send("Hello World")
-
+  response.send("Testing Testing 1, 2, 3")
 })
 
-server.listen(3000, () => {
-  console.log('listening on port 3000')
+server.listen(3003, () => {
+  console.log('listening on port 3003')
 })
