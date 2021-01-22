56282377ee97a07a77291eda023612ffb5326878
GabrielQSherman <gabrielquinn.photography@gmail.com>
2021-01-21 21:40:33 -0500

    move backend server into dedicated directory to organize from client server

---
+++ Projects/APIs/ConnectingFEBE/usersystem-MERN-stack/backend/routes/users.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+const sendErr = require('../middlewares/sendGenericError')
+const User = require('../models/User')
+const { check, validationResult } = require('express-validator')
+
+router.get('/all', async (req, res) => {
+  try {
+    const allUsers = await User.find()
+    res.json({ data: allUsers })
+  } catch (err) {
+    sendErr(err, res)
+  }
+})
+
+router.post(
+  '/signup', 
+  check('email', "Email Is Required").not().isEmpty(),
+  check('username', "Username Is Required").not().isEmpty(),
+  check('password', "Password Is Required").not().isEmpty(),
+  check('email', "Valid Email Required").isEmail(),
+  check('password', "Password must be between 6 and 100 characters").isLength({min: 6, max: 100}),
+  async (req, res) => {
+  const errors = validationResult(req).array()
+  if (errors.length != 0) {
+    return res.status(400).json({errors })
+  }
+  try {
+    const newUser = await User.create(req.body)
+    res.status(201).json(newUser)
+  } catch (err) {
+    sendErr(err, res)
+  }
+})
+
+module.exports = router
---
+++ Projects/APIs/ConnectingFEBE/usersystem-MERN-stack/backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const express = require("express");
+
+const server = express();
+
+const databaseConnect = require('./databaseConnect')
+
+databaseConnect()
+
+server.use(express.json())
+
+
+if (process.env.NODE_ENV === 'development') { 
+    const morgan = require('morgan');
+    server.use(morgan('dev'))
+}
+
+const usersRouter = require('./routes/users');
+server.use('/users', usersRouter)
+
+server.listen(PORT, () => {
+  console.log('listening on port ' + PORT)
+})
+
