67e622752989fbd88893abc05ae63a2994eac5be
GabrielQSherman <gabrielquinn.photography@gmail.com>
2021-01-22 21:35:37 -0500

    create a home componenet

---
+++ Projects/APIs/ConnectingFEBE/usersystem-MERN-stack/frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {homeTitle} from '../config/styles'
+
+export default function Home() {
+  return (
+    <div>
+      <h1
+        style={{...homeTitle}}
+      >
+        Welcome To Our Site
+      </h1>
+    </div>
+  )
+}
