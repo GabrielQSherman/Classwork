6dd5d74a319ada7ca9e674e27a9c259553c5899c
GabrielQSherman <gabrielquinn.photography@gmail.com>
2021-01-22 21:35:25 -0500

    implement Home component into main app component

--- Projects/APIs/ConnectingFEBE/usersystem-MERN-stack/frontend/src/App.js
+++ Projects/APIs/ConnectingFEBE/usersystem-MERN-stack/frontend/src/App.js
@@ -1,21 +1,10 @@
 import React from 'react'
+import Home from './components/Home'
 
 function App() {
-  // const name = 'gabe'
-  const styles = {
-    mainContainer: {
-      backgroundColor: 'pink'
-    }
-  }
+
   return (
-    <div 
-      className="App"//{name} 
-      style={styles.mainContainer}
-    >
-      <h1>
-        Hello world
-      </h1>
-    </div>
+    <Home />
   );
 }
 
