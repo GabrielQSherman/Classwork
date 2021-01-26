62d0731a43c4f604ea7e264a867c855ff91b647e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2021-01-25 22:45:17 -0500

    add new styling

--- Projects/APIs/ConnectingFEBE/usersystem-MERN-stack/frontend/src/config/styles.js
+++ Projects/APIs/ConnectingFEBE/usersystem-MERN-stack/frontend/src/config/styles.js
@@ -1,7 +1,20 @@
 module.exports = {
-  homeTitle: {
+  pageTitle: {
     marginTop: '3%',
     fontSize: 'xxx-large'
+  },
+  defaultNavBtn: {
+    borderRadius: 20,
+    fontSize: 30,
+    padding: 15,
+    cursor: 'pointer',
+    transition: 'all ease-in-out .5s',
+    border: 'solid 2px black',
+    textShadow: '1px 2px black',
+    color: 'white',
+    background: 'none',
+    marginLeft: "3%",
+    marginRight: "3%",
   }
 }
 // export default styles
